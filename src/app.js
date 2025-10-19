39eb82ae4a2b22dd2b888bd99f805ea87e0030d2
jesus-262 <jesus-262@hotmail.com>
2025-10-19 15:40:54 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,16 +6,7 @@ import config from './config.js';
 import { pool } from './db/connection.js';
 dotenv.config();
 const app = express();
-async function testConnection() {
-    try {
-      const connection = await pool.getConnection();
-      console.log('✅ Conexión exitosa a la base de datos');
-      connection.release();
-    } catch (error) {
-      console.error('❌ Error al conectar a la base de datos:', error.message);
-    }
-  }
-  testConnection();
+
 app.use(cors({
     credentials: true,
     ControlAllowCredentials:true,
